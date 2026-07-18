970a983e4676bbd0fe12ad7e4637eae082207277
DEEPIKA B.R <deepika060107@gmail.com>
2026-07-18 10:39:27 +0530

    Configure environment variables and ignore generated files

--- database.py
+++ database.py
@@ -1,13 +1,17 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def connect_db():
     connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root123",
-        database="intellicode"
-    )
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
+)
     return connection
 
 def save_prediction(filename, prediction, probability, risk_level,
